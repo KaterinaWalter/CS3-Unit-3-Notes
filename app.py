7fd611fd94eea0a30b851538b4df1a925617cdb5
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-02-12 13:57:06 +0000

    CSS fun

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ def index():
     name_data = 'Katerina'
     year_data = 2026
     # Can also use lists & dictionaries
-    favorites_list = ['Stardew Valley', 'The Sims', 'BG3', 'Pokemon']
-    ratings_dict = { 'Pokemon' : 'I rate the whole series 10/10.', 
-                     'BG3' : 'Baldurs Gate 3 was GOTY for a reason!', 
-                     'Stardew Valley': 'Endlessly replayable!', 
-                     'The Sims': 'Unplayable without mods, but timeless fun.' }
+    favorites_list = ['Pokemon', 'Baldurs Gate', 'The Sims', 'Zelda' ]
+    ratings_dict = { 'Pokemon Legends Arceus' : 'I love every classic Pokemon game, but the catching mechanic in this one was especially fun.', 
+                     'BG3' : 'Baldurs Gate 3 was Game of the Year for a reason! Got me into the world of D&D.', 
+                     'The Sims 4': 'Unplayable without mods, but timeless fun.',
+                     'Tears of the Kingdom': "My first Zelda game, loved it, and now wishing I've been a fan since the beginning." }
     # name is how we refer to it in the HTML template,
     # name_data is the variable declared here in Python
     return render_template("index.html", name=name_data, year=year_data, favorites=favorites_list, ratings=ratings_dict)
